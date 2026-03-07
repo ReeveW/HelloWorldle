23f79778281f033611c37a4247c237b729912dfd
ReeveW <reeveswilde@gmail.com>
2026-03-07 12:48:26 -0700

    added some languages to test on

--- Languages.h
+++ Languages.h
@@ -1,24 +1,35 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
-enum class Level {
-    High, Low
+enum class Level
+{
+    High,
+    Low
 };
 
-enum class Implementation {
-    Compiled, Interpreted, Hybrid
+enum class Implementation
+{
+    Compiled,
+    Interpreted,
+    Hybrid
 };
 
-enum class MemoryManagement {
-    Manual, GarbageCollected
+enum class MemoryManagement
+{
+    Manual,
+    GarbageCollected
 };
 
-enum class ParentLanguage {
-    C, ABC, 
+enum class ParentLanguage
+{
+    C,
+    ABC,
 };
 
-struct LanguageData {
+struct LanguageData
+{
     std::string name;
     int releaseYear;
     Level level;
@@ -27,4 +38,7 @@ struct LanguageData {
     MemoryManagement mem;
 };
 
-
+std::vector<LanguageData> languages = {{"C++", 1985, Level::Low, 
+    Implementation::Compiled, ParentLanguage::C, MemoryManagement::Manual},
+{"Python", 1991, Level::High, Implementation::Interpreted, ParentLanguage::ABC,
+MemoryManagement::GarbageCollected}};
